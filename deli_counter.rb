cf71fd7306151f3f7821a3acacf495afa452e769
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 19:00:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 
 def line(deli_line)
-  string = "The line is currently: "
+  string = "The line is currently:"
   number = 0;
   if deli_line.length == 0 
     puts "The line is currently empty."
   else  
     while number < deli_line.length
-      string = string + "#{number+1}. #{deli_line[number]} "
+      string = string + " #{number+1}. #{deli_line[number]}"
       number += 1
     end 
     puts string
