0bd984443d5b3f2440d6c7bad3e4d0c3b9c44121
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-11 18:43:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,24 @@
 # Write your code here.
 
 def line(deli_line)
-  string = "The line is currently"
-  number = 1;
-  deli_line.each do |name|
-    
+  string = "The line is currently: "
+  number = 0;
+  while number < deli_line.length
+    string = string + "#{number+1}. #{deli_line[number]}."
+    number += 1
+  end   
+  string
+end
+
+def take_a_number(line, name)
+  position = 0;
+  line.push(name)
+  position = line.length
+  puts "Welcome, #{name}. You are number #{position} in line."
+end
+
+def now_serving(line)
+  name = line.shift
+  puts "Currently serving #{name}."
+end
+
